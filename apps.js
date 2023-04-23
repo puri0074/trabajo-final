e889c2ea4d5e400eeefdd775cc62d5dad9b0440e
Wigner Yerson Puri Eduardo <1472315@senati.pe>
2023-04-23 18:47:09 -0500

    aplicando js para crear filas

--- apps.js
+++ apps.js
@@ -11,3 +11,29 @@ function mostrarOcultarMenu() {
     menu_visible = false;
   }
 }
+//el menu direcciona a cada lugar
+let links = document.querySelectorAll("nav a");
+for (var x = 0; x < links.length; x++) {
+  links[x].onclick = function () {
+    menu.style.display = "none";
+    menu_visible = false;
+  };
+}
+//Creo las barritas de una barra particular identificada por su id
+function crearBarra(id_barra) {
+  for (i = 0; i <= 16; i++) {
+    let div = document.createElement("div");
+    div.className = "e";
+    id_barra.appendChild(div);
+  }
+}
+
+//selecciono todas las barras generales par aluego manipularlas
+let Manejodearmasdeguerra = document.getElementById(
+  "manejo de armas de guerra"
+);
+crearBarra(Manejodearmasdeguerra);
+let Manejodemotos = document.getElementById("Manejo de motos");
+crearBarra(Manejodemotos);
+let Redactardocumentos = document.getElementById("Redactar documentos");
+crearBarra(Redactardocumentos);
